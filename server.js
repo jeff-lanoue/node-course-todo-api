14566211cc5b927b99488c013138ca7270796912
Jeff Lanoue <jeff.lanoue.1@gmail.com>
2019-01-09 21:28:43 -0800

    Updated some more comments for using gits

--- server.js
+++ server.js
@@ -9,3 +9,7 @@
 //git ad
 //git status
 //git commit -m "Init commit"
+//go to git online and make a new reop, there will be
+//an area where I can copy the two lines needed tu
+//put the code there. After that I just need to do
+// git add . , git commit -m "New mesage" ,and git push.
