e52fb028456786014b65e764853bbe91e3a19395
Jeff Lanoue <jeff.lanoue.1@gmail.com>
2019-01-26 15:28:52 -0800

    Added a route that accepts and id parameter

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 //npm i express body-parser --save
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -32,6 +32,26 @@ app.get('/todos', (req, res) => {
    }, (e) => {
         res.status(400).send(e);
    }); 
+});
+
+//GET /todos/122154
+var id = '5c46a0d1d8241c4594448a9a'; // from Todo 
+
+app.get('/todos/:id' , (req, res) => {
+    var id = req.params.id;
+
+    if (! ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }    
+    Todo.findById(id).then((todo) => {
+        if(! todo) {
+            res.send(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });        
+
 });
 
 app.listen(3000, () => {
