8aec959ee2596fe1caa7e438541e59480ec0dd55
Jeff Lanoue <jeff.lanoue.1@gmail.com>
2019-02-25 21:18:54 -0800

    Hash user password before saving to DB

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,28 @@
 //const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+if(1 === 2) {// make equal to create a new hash
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+            console.log(hash);
+        });
+    });
+}
 
-var decoded = jwt.verify(token , '123abc');
-console.log(decoded);
+var hashpassword = '$2a$10$Vk0Br77UC/nrlCjOtijNO.kj57b8/Dcziv1i27d.kct/a4BQoTv4S'
+
+bcrypt.compare(password, hashpassword, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token , '123abc');
+// console.log(decoded);
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var {authenticate} = require('./middleware/authenticate');
+var {authenticate} = require('./middleware/authenticate')  
 
 
 var app = express();
